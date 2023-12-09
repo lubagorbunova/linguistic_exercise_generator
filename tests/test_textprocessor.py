c10658113d549f137b2fa71a0c5e77e1c290481a
Luba <ljuba.gorbunova@gmail.com>
2023-12-09 15:32:31 +0300

    тестик исправлен?

--- tests/test_textprocessor.py
+++ tests/test_textprocessor.py
@@ -6,5 +6,4 @@ class EmployeeBaseTests(unittest.TestCase):
 
     def test_create_employee_ideal(self):
         instance = TextProcessor('кошка спит на диване!')
-        self.assertEqual(instance.get_raw_text())
-        self.assertEqual(instance.get_raw_text())
+        self.assertEqual(instance.get_raw_text(), 'кошка спит на диване!')
