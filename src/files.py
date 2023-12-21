f0b9ca5ad84587ff39d66917ae380fe65f556d6f
Luba <ljuba.gorbunova@gmail.com>
2023-12-21 23:58:47 +0300

    Тесты разделены на несколько файлов. Тесты для класса Files. В классе Files поднимаются ошибки в случае некорректных файлов.

--- src/files.py
+++ src/files.py
@@ -14,6 +14,24 @@ def prepare_environment(base_path: Union[Path, str]) -> None:
     base_path.mkdir(parents=True)
 
 
+class EmptyFileError(BaseException):
+    """
+    Файл пустой.
+    """
+
+
+class NoFileError(BaseException):
+    """
+    Файл не указан.
+    """
+
+
+class NothingToWriteError(BaseException):
+    """
+    В файл ничего не записывается.
+    """
+
+
 class Files:
     def __init__(self, file_name: str):
         self.file_name = file_name
@@ -21,12 +39,17 @@ class Files:
 
     def read_file(self) -> str:
         with open(self.text_path, encoding='utf-8') as file:
-            return file.read()
+            text = file.read()
+        if len(text)==0:
+            raise EmptyFileError
+        return text
 
     def get_exercises_path(self) -> Path:
         """
         Returns path for requested exercise
         """
+        if self.file_name == '':
+            raise NoFileError
         exercise_name = f"{self.file_name[:self.file_name.index('.')]}_exercises.txt"
         return ASSETS_PATH / exercise_name
 
@@ -34,6 +57,8 @@ class Files:
         """
         Returns path for requested exercise
         """
+        if self.file_name == '':
+            raise NoFileError
         answer_name = f"{self.file_name[:self.file_name.index('.')]}_answers.txt"
         return ASSETS_PATH / answer_name
 
@@ -42,6 +67,8 @@ class Files:
         записывает упражнения и ответы в файлы, которые передаются пользователю
         :return:
         """
+        if (len(ex_text) == 0) or (len(answer_text) == 0):
+            raise NothingToWriteError
         with open(self.get_exercises_path(), 'w', encoding='utf-8') as file_1:
             file_1.write(ex_text)
         with open(self.get_answers_path(), 'w', encoding='utf-8') as file_2:
---
+++ tests/test_files.py
@@ -0,0 +1,52 @@
+import unittest
+from src.files import Files, EmptyFileError, NoFileError, NothingToWriteError
+import shutil
+from pathlib import Path
+from src.constants import ASSETS_PATH
+from typing import Union
+
+
+class FilesBaseTests(unittest.TestCase):
+    def test_readfile_content(self):
+        instance = Files('fortest.txt')
+        self.assertEqual(instance.read_file(), 'Кошка спит на диване.')
+
+    def test_readfile_notempty(self):
+        instance = Files('fortestempty.txt')
+        with self.assertRaises(EmptyFileError):
+            instance.read_file()
+
+    def test_getexercisepass(self):
+        instance = Files('fortest.txt')
+        exercise_name = f"{instance.file_name[:instance.file_name.index('.')]}_exercises.txt"
+        self.assertEqual(instance.get_exercises_path(), ASSETS_PATH /exercise_name)
+
+    def test_getexercisepass_nofilename(self):
+        instance = Files('')
+        with self.assertRaises(NoFileError):
+            instance.get_exercises_path()
+
+    def test_get_answerspass(self):
+        instance = Files('fortest.txt')
+        answers_name = f"{instance.file_name[:instance.file_name.index('.')]}_answers.txt"
+        self.assertEqual(instance.get_answers_path(), ASSETS_PATH / answers_name)
+
+    def test_get_answerspass_incorrect(self):
+        instance = Files('')
+        with self.assertRaises(NoFileError):
+            instance.get_answers_path()
+
+    def test_writetofile_content(self):
+        instance = Files('fortest.txt')
+        instance.write_to_file('вопросы', 'ответы')
+        with open(ASSETS_PATH / 'fortest_exercises.txt', encoding='utf-8') as file:
+            ex = file.read()
+        self.assertEqual('вопросы', ex)
+        with open(ASSETS_PATH / 'fortest_answers.txt', encoding='utf-8') as file:
+            answers = file.read()
+        self.assertEqual('ответы', answers)
+
+    def test_writetofile_incorrect(self):
+        instance = Files('fortest.txt')
+        with self.assertRaises(NothingToWriteError):
+            instance.write_to_file('', '')
